e2db7036d6d263b08b391873857a2be6e51074cc
Alejandro <AlexMJHS@gmail.com>
2016-02-17 09:51:03 -0700

    Added toString

--- src/recursion/model/CodeTimer.java
+++ src/recursion/model/CodeTimer.java
@@ -3,7 +3,7 @@ package recursion.model;
 /**
  * 
  * @author Alejandro Magallanes
- * @version 0.1 Feb 17, 2016
+ * @version 0.1 Feb 17, 2016 Added basic methods and constructor.
  */
 
 public class CodeTimer
@@ -29,4 +29,20 @@ public class CodeTimer
 	{
 		this.executionTime = 0;
 	}
+
+	/**
+	 * @return The executionTime value for the CodeTimer object.
+	 */
+	public long getExecutionTime()
+	{
+		return executionTime;
+	}
+	
+	public String toString()
+	{
+		String timerDescription = "Current execution time is : " + this.executionTime + " in nanoseconds.";
+		
+		return timerDescription;
+	}
+	
 }
